246566b618211b7809d7ab9efdbe42f731de0450
mikamore <kim.roman-2002@yandex.ru>
2022-12-18 18:05:29 +0300

    Refactormodel

--- kortex/chats/models.py
+++ kortex/chats/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from users.models import User
 
-# Create your models here.
 
 class Chat(models.Model):
-    members = models.ManyToManyField(User, verbose_name="members", related_name='chats')
+    members = models.ManyToManyField(User, verbose_name="members", related_name="chats")
+
 
 class Message(models.Model):
-    author = models.ForeignKey(User, verbose_name="author", on_delete=models.CASCADE)
-    chat = models.ForeignKey(Chat, verbose_name="chat", on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, verbose_name="author", on_delete=models.CASCADE, related_name="messages"
+    )
+    chat = models.ForeignKey(
+        Chat, verbose_name="chat", on_delete=models.CASCADE, related_name="messages"
+    )
     text = models.TextField(verbose_name="text")
